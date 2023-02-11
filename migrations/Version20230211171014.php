0a22e671817e558660f63c0754d9f55e0b6ea783
valouz23 <v.djamei@ecole-ipssi.net>
2023-02-11 18:12:40 +0100

    Ajout d'une migration

---
+++ migrations/Version20230211171014.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230211171014 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE cadavre (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, phrase VARCHAR(255) NOT NULL, INDEX IDX_7B1F8E53A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE francais (id INT AUTO_INCREMENT NOT NULL, groupe_de_mots VARCHAR(255) NOT NULL, fonction VARCHAR(255) NOT NULL, personne VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE cadavre ADD CONSTRAINT FK_7B1F8E53A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE cadavre DROP FOREIGN KEY FK_7B1F8E53A76ED395');
+        $this->addSql('DROP TABLE cadavre');
+        $this->addSql('DROP TABLE francais');
+        $this->addSql('DROP TABLE user');
+    }
+}

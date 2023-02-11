59a6f6c9f90dda5049fbd82c26c6996ab162a879
valouz23 <v.djamei@ecole-ipssi.net>
2023-02-11 16:55:10 +0100

    Ajout de l'entité cadavre et relation avec l'user

---
+++ src/Entity/Cadavre.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CadavreRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CadavreRepository::class)]
+class Cadavre
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $phrase = null;
+
+    #[ORM\ManyToOne(inversedBy: 'Cadavres')]
+    private ?User $User = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPhrase(): ?string
+    {
+        return $this->phrase;
+    }
+
+    public function setPhrase(string $phrase): self
+    {
+        $this->phrase = $phrase;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->User;
+    }
+
+    public function setUser(?User $User): self
+    {
+        $this->User = $User;
+
+        return $this;
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -26,6 +28,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     #[ORM\Column]
     private ?string $password = null;
+
+    #[ORM\OneToMany(mappedBy: 'User', targetEntity: Cadavre::class)]
+    private Collection $Cadavres;
+
+    public function __construct()
+    {
+        $this->Cadavres = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -95,5 +105,35 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    /**
+     * @return Collection<int, Cadavre>
+     */
+    public function getCadavres(): Collection
+    {
+        return $this->Cadavres;
+    }
+
+    public function addCadavre(Cadavre $cadavre): self
+    {
+        if (!$this->Cadavres->contains($cadavre)) {
+            $this->Cadavres->add($cadavre);
+            $cadavre->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCadavre(Cadavre $cadavre): self
+    {
+        if ($this->Cadavres->removeElement($cadavre)) {
+            // set the owning side to null (unless already changed)
+            if ($cadavre->getUser() === $this) {
+                $cadavre->setUser(null);
+            }
+        }
+
+        return $this;
     }
 }
